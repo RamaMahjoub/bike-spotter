3ab33382056e1661a232b6a833c93da1d8e016c2
RamaMahjoub <ramamhrama093@gmail.com>
2024-06-03 03:16:33 +0300

    create theft cases list content component

---
+++ src/main/app/theft-cases/theft-cases-list/Content.tsx
@@ -0,0 +1,64 @@
+import { usePagination } from '@core/hooks'
+import CircularProgress from '@mui/material/CircularProgress'
+import List from '@mui/material/List'
+import Stack from '@mui/material/Stack'
+import { useAppSelector } from 'main/store/index'
+import { useCallback, useEffect } from 'react'
+import PaginationComponent from '../../components/Pagination'
+import useFetchTheftCases from '../hooks/useFetchTheftCases'
+import {
+    selectAllCasesData,
+    selectAllCasesStatus,
+    selectCasesCount
+} from '../store/theftCasesSlice'
+import { TheftCaseItem } from '../types/responses/TheftCaseItem'
+import CaseItem from './components/CaseItem'
+import FilterBar from './components/FilterBar'
+
+const Content = () => {
+    const { page, handlePageChange } = usePagination()
+    const total = useAppSelector(selectCasesCount)
+    const fetchTheftCases = useFetchTheftCases(page)
+    const cases = useAppSelector(selectAllCasesData)
+    const fetchTheftCasesStatus = useAppSelector(selectAllCasesStatus)
+
+    useEffect(() => {
+        fetchTheftCases()
+    }, [fetchTheftCases])
+
+    const renderCaseItem = useCallback((caseItem: TheftCaseItem) => {
+        return (
+            <CaseItem
+                key={caseItem.id}
+                caseItem={caseItem}
+            />
+        )
+    }, [])
+
+    return (
+        <Stack gap={4}>
+            <FilterBar />
+            <List>
+                {fetchTheftCasesStatus === 'loading' ? (
+                    <CircularProgress
+                        color="secondary"
+                        size={20}
+                    />
+                ) : fetchTheftCasesStatus === 'idle' ? (
+                    []
+                ) : (
+                    cases.bikes.map((bike: TheftCaseItem) =>
+                        renderCaseItem(bike)
+                    )
+                )}
+            </List>
+            <PaginationComponent
+                handleChange={handlePageChange}
+                page={page}
+                total={total || 0}
+            />
+        </Stack>
+    )
+}
+
+export default Content
