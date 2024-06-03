c30605362a57bb508db6553528a425a615f5fbbb
RamaMahjoub <ramamhrama093@gmail.com>
2024-06-03 17:08:56 +0300

    add getById api

--- src/main/app/theft-cases/services/config.ts
+++ src/main/app/theft-cases/services/config.ts
@@ -1,4 +1,5 @@
 export const TheftCasesServiceConfig = {
     getAll: '/v3/search',
-    getDataCount: '/v3/search/count'
+    getDataCount: '/v3/search/count',
+    getDataById: (id: number) => `/v3/bikes/${id}`
 }
